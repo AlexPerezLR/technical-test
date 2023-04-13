c0d247de2bc15b657e454712d2510d5284918036
Alex Perez <alejandroperez.info@gmail.com>
2023-04-13 14:51:09 +0200

    Add brands and products data

---
+++ src/main/resources/db/migration/V3_1__fill_products_table.sql
@@ -0,0 +1,4 @@
+insert into public.product (id, name, registration_date, deletion_date)
+values (35455, 'Camiseta blanca', '2023-04-13 14:35:00', null),
+       (35454, 'Top beige','2023-04-13 14:35:00', null),
+       (25455, 'Pantalones vaqueros azul','2023-04-13 14:35:00', null);
